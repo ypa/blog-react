36e34ff7a27da142be36013031627a58200a6773
Yan P Aung <yan.pye.aung@gmail.com>
2020-09-26 09:17:58 -0700

    Fetch data from APIs and hook them to the redux thunk dispatch and display them inside components. Also use Lodash to solve overfetching issue with fetchUsers. Completed steps: 	Step 7: Some reducer sees the action, returns the data off of the 'payload' 	Step 8: Because we generated some new state object, redux/react-redux cause our React app to be rerendered

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,35 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get('/posts');
 
-  dispatch( { type: 'FETCH_POSTS', payload: response });
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+/*** Memoization using Lodash approach to solve fetchUser
+ *  getting called multiple times (overfetching issue) for the same user
+ *  Commenting out for now in favor of fetchPostsAndUsers approach above
+*/
+/*
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+});
+*/
